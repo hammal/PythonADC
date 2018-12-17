1819d03e72d03e0a9ccfcb11d5e06fecf1db9c66
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2018-12-17 21:03:11 +0100

    try leftsided average

--- AnalogToDigital/evaluation.py
+++ AnalogToDigital/evaluation.py
@@ -343,8 +343,8 @@ class SigmaDeltaPerformance(object):
         for offset in range(1,  np.minimum(maxPeakNeighbor, np.minimum(lowerIndexBound, upperIndexBound))):
             peakChange = tempSpec[index + offset] + tempSpec[index - offset]
             peakHeight += peakChange
-            # avgHeight = (np.mean(tempSpec[index-offset:]) + np.mean(tempSpec[:index+offset+1])) / 2.
-            avgHeight = (np.mean(tempSpec[:index-offset]) + np.mean(tempSpec[index+offset+1:])) / 2.
+            # avgHeight = (np.mean(tempSpec[:index-offset]) + np.mean(tempSpec[index+offset+1:])) / 2.
+            avgHeight = (np.mean(tempSpec[:index-offset]))# + np.mean(tempSpec[index+offset+1:])) / 2.
             diff = np.abs(peakChange/2. - avgHeight)/avgHeight
             ratio = peakChange/(2 * avgHeight)
             print(diff, ratio, offset, peakChange / 2, avgHeight)
