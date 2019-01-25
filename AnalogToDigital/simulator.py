6d99bd6f543b8735777390420edf1baa9fe97816
Ólafur Jón Thoroddsen <olafurt@student.ethz.ch>
2019-01-25 18:12:09 +0100

    Write OOB states to the log before clipping

--- AnalogToDigital/simulator.py
+++ AnalogToDigital/simulator.py
@@ -139,12 +139,11 @@ class Simulator(object):
                 above = self.state > bound
                 below = self.state < -bound
 
-                self.state[above] = bound
-                self.state[below] = -bound
-
                 oob_states = np.arange(self.system.order)[np.logical_or(above,below)]
                 if any(oob_states):
                     self.log("STATE BOUND EXCEEDED! X_{} = {}".format(oob_states, self.state[oob_states]))
+                    self.state[above] = bound
+                    self.state[below] = -bound
 
             # print(self.state)
             self.control.update(self.state)
