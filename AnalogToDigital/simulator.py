1be9fa09dfd986ddb1faa14ac4929ff1a279dc7f
Ólafur Jón Thoroddsen <olafurt@student.ethz.ch>
2019-06-14 13:14:32 +0000

    Work in progress: speeding up the simulator

--- AnalogToDigital/simulator.py
+++ AnalogToDigital/simulator.py
@@ -78,7 +78,7 @@ class Simulator(object):
 
                 # Print progress every 1e4 samples
                 try:
-                    if current_sample % (num_samples//1e4) == 0:
+                    if current_sample % 1e4 == 0:
                         print("Simulation Progress: %.2f%%    \r" % (100*(current_sample/num_samples)), end='', flush=True)
                 except ZeroDivisionError:
                     pass
